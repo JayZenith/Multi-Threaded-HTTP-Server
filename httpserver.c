80d4a055734166ce3e0c695048b8ef6f99edf374
JayZenith <162098309+JayZenith@users.noreply.github.com>
2024-03-05 19:41:17 -0800

    Implementing the worker threads

---
+++ httpserver.c
@@ -0,0 +1,395 @@
+#include <assert.h>
+#include <err.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+
+#include "asgn2_helper_funcs.h"
+#include <stdbool.h>
+#include <ctype.h>
+#include <regex.h>
+#include <pthread.h>
+
+#include "queue.h"
+
+typedef struct{
+    int thread_id;
+    pthread_t *thread;
+} reqThread;
+
+#define BUF_SIZE 1000000
+static reqThread **thread_pool;
+#define DEFAULT_THREAD_COUNT 4;
+#define OPTIONS "t:l:"
+static FILE *logfile;
+queue_t *q;
+
+void fatal_error(const char *msg) {
+    fprintf(stderr, "%s\n", msg);
+    exit(1);
+}
+
+typedef struct Request {
+    int sock;
+    char meth[20];
+    char uri[100];
+    char vers[20];
+    char hdr[100];
+    unsigned int val;
+} Request;
+
+void getOutput(int connn, char *b, size_t len) {
+    size_t num_written = 0;
+
+    write_n_bytes(connn, b + num_written, len);
+}
+
+void processPUT(Request *req, char *bufm, int conny, int signal) {
+
+    char response[150] = { 0 };
+    if ((strcmp(req->vers, "HTTP/1.1") != 0) || signal == 1) {
+        sprintf(response, "HTTP/1.1 400 Bad Request\r\nContent-Length: %d\r\n\r\nBad Request\n",
+            req->val);
+        getOutput(conny, response, strlen(response));
+        return;
+    }
+
+    int fd = open(req->uri, O_WRONLY | O_TRUNC, 0666);
+    if (fd == -1) { //FILE DONT EXIST
+        fd = open(req->uri, O_WRONLY | O_TRUNC | O_CREAT, 0666);
+        if (fd == -1) {
+            printf("creating error");
+        }
+        sprintf(response, "HTTP/1.1 201 Created\r\nContent-Length: 8\r\n\r\nCreated\n");
+        getOutput(conny, response, strlen(response));
+    } else { //YOURE NOT WRITING
+        sprintf(response, "HTTP/1.1 200 OK\r\nContent-Length: 3\r\n\r\nOK\n");
+        getOutput(conny, response, strlen(response));
+    }
+    //just need to figure out flags and make this PUT shit work
+
+    //WRITE WHAT WAS IN THE BUFFER INITIALLY
+    size_t num_written = 0;
+    write_n_bytes(fd, bufm + num_written, (size_t) req->val);
+
+    //NOW CONTINUE READING AND WRITING VIA SOCKET
+    size_t bytez = 0;
+    size_t num_read = 0;
+    size_t free_space = sizeof(bufm);
+
+    for (;;) {
+        bytez = read(conny, bufm + num_read, free_space);
+        if (bytez < 0)
+            printf("bytez error");
+        if (bytez == 0)
+            break;
+        num_read += bytez;
+        free_space -= bytez;
+        if (free_space == 0) {
+            //getOutput(conny, bufm + 0, num_read);
+            write_n_bytes(fd, bufm + 0, (size_t) req->val);
+            free_space = sizeof(bufm);
+            num_read = 0;
+        } else {
+            getOutput(fd, bufm + 0, num_read);
+        }
+    }
+
+    close(fd);
+}
+
+void processGet(Request *req, int conny, int signal) {
+    char response[100] = { 0 };
+    if ((strcmp(req->vers, "HTTP/1.1") != 0) || signal == 1) {
+        sprintf(response, "HTTP/1.1 505 Version Not Supported\r\nContent-Length: 22\r\n\r\nVersion "
+                          "Not Supported\n");
+        getOutput(conny, response, strlen(response));
+        return;
+    }
+
+    if (access(req->uri, X_OK) == 0) {
+        sprintf(response, "HTTP/1.1 403 Forbidden\r\nContent-Length: 10\r\n\r\nForbidden\n");
+        getOutput(conny, response, strlen(response));
+        return;
+    }
+
+    int fd = open(req->uri, O_RDONLY, 0666);
+    if (fd == -1) {
+        sprintf(response, "HTTP/1.1 404 Not Found\r\nContent-Length: 10\r\n\r\nNot Found\n");
+        getOutput(conny, response, strlen(response));
+        close(fd);
+        return;
+    }
+
+    char buf[BUF_SIZE] = { 0 };
+    size_t free_space = sizeof(buf);
+    size_t num_read = 0;
+
+    struct stat finfo;
+    fstat(fd, &finfo);
+    off_t fileSize = finfo.st_size;
+
+    //printf("\n%s%zu\n", "davis", (size_t)fileSize);
+
+    sprintf(response, "HTTP/1.1 200 OK\r\nContent-Length: %zu\r\n\r\n", (size_t) fileSize);
+    getOutput(conny, response, strlen(response));
+
+    size_t bytez = 0;
+
+    for (;;) {
+        bytez = read(fd, buf + num_read, free_space);
+        if (bytez < 0)
+            printf("bytez error");
+        if (bytez == 0)
+            break;
+        num_read += bytez;
+        free_space -= bytez;
+        if (free_space == 0) {
+            getOutput(conny, buf + 0, num_read);
+            free_space = sizeof(buf);
+            num_read = 0;
+        } else {
+            getOutput(conny, buf + 0, num_read);
+        }
+    }
+
+    //num_read = read_until(fd, buf + num_read, free_space, "");
+    //sprintf(response, "HTTP/1.1 200 OK\r\nContent-Length: %zu\r\n\r\n", num_read);
+    //getOutput(conny, response, strlen(response));
+    //getOutput(conny, buf + 0, num_read);
+
+    close(fd);
+    return;
+}
+
+#define REQLINE "^([A-Za-z]{1,8}) /([A-Za-z0-9_.-]{2,64}) (HTTP/[0-9]\\.[0-9])\r\n\0$"
+#define HEAD    "^([A-Za-z0-9_.-]{1,28}:) ([ -~]{1,128})\r\n$"
+#define HEAD2   "^(([A-Za-z0-9_.-]{1,28}:) ([ -~]{1,128})\r\n)*\0$"
+
+void processReq(Request *req, char *bufa, int con) {
+    regex_t preg = { 0 };
+    regmatch_t p[4] = { 0 };
+    char response[100];
+    if ((regcomp(&preg, REQLINE, REG_EXTENDED)) != 0) {
+        sprintf(response, "HTTP/1.1 400 Bad Request\r\nContent-Length: 12\r\n\r\nBad Request\n");
+        getOutput(con, response, strlen(response));
+        return;
+    }
+
+    if ((regexec(&preg, bufa, (size_t) 4, p, 0)) != 0) {
+        sprintf(response, "HTTP/1.1 400 Bad Request\r\nContent-Length: 12\r\n\r\nBad Request\n");
+        getOutput(con, response, strlen(response));
+        return;
+    } else { //METHOD URI VERSION
+        //METHOD
+        memcpy(req->meth, &bufa[p[1].rm_so], p[1].rm_eo);
+        req->meth[p[1].rm_eo] = '\0';
+        //URI
+        int d = p[2].rm_eo - p[2].rm_so;
+        memcpy(req->uri, &bufa[p[2].rm_so], d);
+        req->uri[d] = '\0';
+        //VERSION
+        int k = p[3].rm_eo - p[3].rm_so;
+        memcpy(req->vers, &bufa[p[3].rm_so], k);
+        req->vers[k] = '\0';
+    }
+
+    bufa += p[0].rm_eo; //point after \r\n
+    if (bufa[3] == '\0') {
+        if (strcmp(req->meth, "GET") == 0) { //GET /..txt HTTP/#.#\r\n\r\n\0
+            processGet(req, con, 0);
+            return;
+        } else if (strcmp(req->meth, "PUT") == 0) { //PUT /..txt HTTP/#.#\r\n\r\n\0
+            processPUT(req, bufa, con, 1);
+            return;
+        } else {
+            sprintf(response,
+                "HTTP/1.1 501 Not Implemented\r\nContent-Length: 16\r\n\r\nNot Implemented\n");
+            getOutput(con, response, strlen(response));
+            return;
+        }
+    }
+
+    //HEADER
+    regex_t preg2 = { 0 };
+    regmatch_t p2[4] = { 0 };
+    if ((regcomp(&preg2, HEAD2, REG_EXTENDED)) != 0)
+        fatal_error("regcomp error");
+    if ((regexec(&preg2, bufa, (size_t) 4, p2, 0)) != 0) {
+        processGet(req, con, 0);
+    }
+
+    memcpy(req->hdr, &bufa[p2[0].rm_so], p2[0].rm_eo);
+    req->hdr[p2[0].rm_eo] = '\0';
+    req->val = atol(&bufa[p2[3].rm_so]);
+
+    bufa += p2[0].rm_eo; //Now point before \r\n then msgBody
+    //1. if bufa[3] == null then check if get or put to determine course of action
+    if (bufa[3] == '\0') {
+        if (strcmp(req->meth, "GET") == 0) { //GET /...txt HTTP/#.#\r\nContent-Length: #\r\n\r\n\0
+            processGet(req, con, 0);
+            return;
+        } else if (strcmp(req->meth, "PUT")
+                   == 0) { //PUT /...txt HTTP/#.#\r\nContent-Length: #\r\n\r\n\0
+            processPUT(req, bufa, con, 1);
+            return;
+        } else {
+            return; //FIX THIS
+        }
+    } else {
+        if (strcmp(req->meth, "GET")
+            == 0) { //GET /...txt HTTP/#.#\r\nContent-Length: #\r\n\r\nMESSAGE
+            processGet(req, con, 1);
+            return;
+        } else if (strcmp(req->meth, "PUT")
+                   == 0) { //PUT /...txt HTTP/#.#\r\nContent-Length: #\r\n\r\nMESSAGE
+            bufa += 2;
+            processPUT(req, bufa, con, 0);
+            return;
+        } else {
+            return; //FIX THIS
+        }
+    }
+}
+
+void handle_connection(int connfd) {
+    char buffer[1000000] = { '\0' };
+    //memset(buffer, 0, 1000);
+    ssize_t bytes = 0;
+    Request req = { 0 };
+
+    while ((bytes = read_until(connfd, buffer, 1000000, "")) > 0) {
+        processReq(&req, buffer, connfd);
+    }
+    exit(1);
+    if (errno == EAGAIN || errno == EWOULDBLOCK) {
+        char response[70];
+        sprintf(response, "HTTP/1.1 400 Bad Request\r\nContent-Length: 12\r\n\r\nBad Request\n");
+        getOutput(connfd, response, strlen(response));
+    }
+
+    (void) connfd;
+    close(connfd);
+    return;
+}
+
+static void usage(char *exec){
+    fprintf(stderr, "usage: %s [-t threads] [-l logfile] <port>\n", exec);
+}
+
+static size_t strtouint16(char number[]) {
+    char *last;
+    long num = strtol(number, &last, 10);
+    if (num <= 0 || num > UINT16_MAX || *last != '\0') {
+        return 0;
+    }
+    return num;
+}
+
+
+
+void *worker_thread(void *arg){
+        //char buffer[1000000] = { '\0' };
+        //ssize_t bytes = 0;
+        (void) arg;
+        uintptr_t connfd3;
+        int conny;
+        for(;;){
+            queue_pop(q, (void**)&connfd3); //then pop it to display it to make sure working 
+            conny = (int)connfd3;
+            printf("\n\n%s%d\n\n", "is it:", conny); //negative?
+            /*
+            while ((bytes = read_until(conny, buffer, 1000000, "")) > 0) {
+                //processReq(&req, buffer, connfd);
+            }
+            for(int i = 0; i < 18; i++)
+                printf("%c", buffer[i]);
+            printf("end");
+            exit(1);
+            */
+            handle_connection(conny);
+        }
+        return NULL;
+}
+
+
+int main(int argc, char *argv[]) {
+    int opt = 0;
+    int threads = DEFAULT_THREAD_COUNT;
+    logfile = stderr;
+
+    while((opt = getopt(argc, argv, OPTIONS)) != -1){
+        switch(opt){
+            case 't':
+                printf("\n%s%s\n", "optarg:", optarg);
+                threads = strtol(optarg, NULL, 10);
+                //if(threads <= 0){
+                    //errx(EXIT_FAILURE, "wrong number of threads");
+                //}
+                break; 
+            return EXIT_FAILURE;
+        }
+    } 
+
+    if(optind >= argc){
+        warnx("wrong number of args");
+        usage(argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    int port = strtouint16(argv[optind]);
+    if(port == 0){
+        errx(EXIT_FAILURE, "bad port number: %s", argv[1]);
+    }
+    printf("\n%s%d\n", "port:", port);
+
+    q = queue_new(threads); //to hold enough connfds
+
+    thread_pool = (reqThread **)malloc(sizeof(reqThread *)*threads); //depends on thread count 
+
+    for(int i = 0; i < threads; i++){
+        thread_pool[i] = (reqThread *)malloc(sizeof(reqThread));
+        thread_pool[i]->thread_id = i;
+        thread_pool[i]->thread = (pthread_t *)malloc(sizeof(pthread_t));
+        pthread_create(thread_pool[i]->thread, NULL, worker_thread, NULL);
+    }
+    
+
+    Listener_Socket listenfd;
+
+    if ((listener_init(&listenfd, port)) == -1)
+        errx(EXIT_FAILURE, "not listening");
+
+    //signal(SIGPIPE, SIG_IGN);
+
+    errno = 0;
+    while (1) { //just place connections in queue 
+        int connfd = listener_accept(&listenfd);
+        if (connfd < 0) {
+            warn("accept error");
+            continue;
+        }
+        //connfd = 444;
+        int64_t con = connfd;
+
+        //uintptr_t num;
+
+        //handle_connection(connfd);
+        queue_push(q, (void *)(intptr_t)con); //should push this connfd 
+        //queue_pop(q, (void**)&num); //then pop it to display it to make sure working 
+        //printf("\n\n%s%lu\n\n", "is it:", num);
+    }
+    return EXIT_SUCCESS;
+    
+}
